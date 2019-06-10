00368926f341668062aa45498cfb87e87af07bfc
Jonathan Macías <jonathan.macias@qbit.com.ec>
2019-06-10 00:30:40 -0500

    primeras pruebas

---
+++ Assets/Scripts/caminarPersonaje.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caminarPersonaje : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)){
+            transform.Translate(Time.deltaTime*5, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.Translate(-Time.deltaTime*5, 0,0);
+        }
+        if(Input.GetKey(KeyCode.W)){
+            transform.Translate(0, Time.deltaTime*5, 0);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/seguirPersonaje.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seguirPersonaje : MonoBehaviour
+{
+    public Transform personaje;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(personaje.position.x, transform.position.y, transform.position.z);
+    }
+}
