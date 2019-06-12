afaee6222b4251cfb688f407c438b83d658ef0fc
Jonathan Macías <jonathan.macias@qbit.com.ec>
2019-06-12 09:02:27 -0500

    mecanica nueva

--- Assets/Scripts/caminarPersonaje.cs
+++ Assets/Scripts/caminarPersonaje.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class caminarPersonaje : MonoBehaviour
 {
+    Animator myAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //myAnimation = GetComponent<Animator>();
     }
 
     void FixedUpdate(){
@@ -18,12 +20,13 @@ public class caminarPersonaje : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D)){
             transform.Translate(Time.deltaTime*5, 0, 0);
+            //myAnimation.setFloat("Speed", Input.GetAxis("Horizontal"));
         }
         if(Input.GetKey(KeyCode.A)){
             transform.Translate(-Time.deltaTime*5, 0,0);
         }
         if(Input.GetKey(KeyCode.W)){
-            transform.Translate(0, Time.deltaTime*5, 0);
+            transform.Translate(0, Time.deltaTime*12, 0);
         }
         
     }
--- Assets/Scripts/seguirPersonaje.cs
+++ Assets/Scripts/seguirPersonaje.cs
@@ -9,6 +9,7 @@ public class seguirPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(personaje.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(personaje.position.x, personaje.position.y, transform.position.z);
+        
     }
 }
